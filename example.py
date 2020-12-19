ebbeb4793c5422adcb1daf3f6a9f411df4bb54bc
edgard <62401294+psded@users.noreply.github.com>
2020-12-19 21:10:18 +0300

    Update example.py

--- example.py
+++ example.py
@@ -10,9 +10,7 @@ def success(resp):  # Success handler to prove it's async
     print(resp)
 
 
-prx = "socks5://3405kp:0u45dM@194.124.49.104:8000"
-
 reqs = [gh.get(f"https://ya.ru?i={i}") for i in range(10)]  # Get google 10 times with different parameter values
 start = perf_counter()  # Start "stopwatch"
-resp = gh.map(reqs, proxies=prx)  # Map request (execute)
+resp = gh.map(reqs)  # Map request (execute)
 print("GachiHTTP:", perf_counter() - start)  # Print time
